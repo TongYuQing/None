1a698954f86951178cc0c21d00712022f4d4bb9a
TongYuQing <191405437@qq.com>
2018-08-18 02:54:13 +0800

    Update project3.py

--- project3.py
+++ project3.py
@@ -8,8 +8,8 @@ import time
 img_path = '/home/tong/图片/Wallpapers'
 
 # 将所有图片resize成 w * h
-w = 112
-h = 96
+w = 128
+h = 128
 c = 3
 
 # 读取图片
@@ -45,7 +45,7 @@ x_val = data[a:]
 y_val = label[a:]
 
 # 批量读取数据进行训练
-def  minbatches(inputs=None, targets=None, batch_size=None, shuffle=False):
+def  minibatches(inputs=None, targets=None, batch_size=None, shuffle=False):
     assert len(targets) == len(targets)
     if shuffle:
         indices = np.arange(len(inputs))
@@ -61,7 +61,7 @@ def  minbatches(inputs=None, targets=None, batch_size=None, shuffle=False):
 #
 #
 x = tf.placeholder(tf.float32,shape=[None,w,h,c], name='x')
-y_ = tf.placeholder(tf.int32, shape=[None,], neme='y_')
+y_ = tf.placeholder(tf.int32, shape=[None,], name='y_')
 
 
 def CNNlayer():
@@ -124,3 +124,47 @@ def CNNlayer():
                             kernel_initializer=tf.truncated_normal_initializer(stddev=0.01),
                             kernel_regularizer=tf.contrib.layers.l2_regularizer(0.003))
     return logits
+
+logits = CNNlayer()
+loss=tf.losses.sparse_softmax_cross_entropy(labels=y_,logits=logits)
+train_op=tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
+correct_prediction = tf.equal(tf.cast(tf.argmax(logits,1),tf.int32), y_)    
+acc= tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+#选取多次迭代中测试集精度最高的三次进行保存
+saver=tf.train.Saver(max_to_keep=3)
+
+max_acc=0
+f=open('/home/tong/图片/Wallpapers/acc.txt', 'w')
+ 
+ 
+n_epoch = 10
+batch_size = 64
+sess=tf.InteractiveSession()  
+sess.run(tf.global_variables_initializer())
+for epoch in range(n_epoch):
+    start_time = time.time()
+    
+    #training
+    train_loss, train_acc, n_batch = 0, 0, 0
+    for x_train_a, y_train_a in minibatches(x_train, y_train, batch_size, shuffle=True):
+        _,err,ac=sess.run([train_op,loss,acc], feed_dict={x: x_train_a, y_: y_train_a})
+        train_loss += err; train_acc += ac; n_batch += 1
+    print("   train loss: %f" % (train_loss/ n_batch))
+    print("   train acc: %f" % (train_acc/ n_batch))
+    
+    #validation
+    val_loss, val_acc, n_batch = 0, 0, 0
+    for x_val_a, y_val_a in minibatches(x_val, y_val, batch_size, shuffle=False):
+        err, ac = sess.run([loss,acc], feed_dict={x: x_val_a, y_: y_val_a})
+        val_loss += err; val_acc += ac; n_batch += 1
+    print("   validation loss: %f" % (val_loss/ n_batch))
+    print("   validation acc: %f" % (val_acc/ n_batch))
+ 
+    f.write(str(epoch+1)+', val_acc: '+str(val_acc)+'\n')
+    if val_acc>max_acc:
+        max_acc=val_acc
+        saver.save(sess,'/home/tong/faces.ckpt',global_step=epoch+1)
+ 
+f.close()
+sess.close()
